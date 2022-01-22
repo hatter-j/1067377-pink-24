f7edc6894af106d69401b414a6c2f452eb985ed5
hatter-j <hatterjulia@mail.ru>
2022-01-23 02:48:00 +0300

    Декоративные элементы главной страницы

--- source/js/page-header-toggle.js
+++ source/js/page-header-toggle.js
@@ -1,6 +1,14 @@
 var pageHeader = document.querySelector('.page-header');
 var navToggle = document.querySelector('.page-header__toggle');
 
+pageHeader.classList.remove('page-header--nojs');
+
 navToggle.addEventListener('click', function() {
-  pageHeader.classList.toggle('page-header--closed');
+  if (pageHeader.classlist.contains("page-header--closed")) {
+    pageHeader.classList.remove("page-header--closed");
+    pageHeader.classList.add("page-header--opened");
+  } else {
+    pageHeader.classList.add("page-header--closed");
+    pageHeader.classlist.remove("page-heder--opened");
+  }
 });
